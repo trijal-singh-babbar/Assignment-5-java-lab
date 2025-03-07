51978886725f39599dac4f8a50e16e023aeff415
trijal-singh-babbar <trijalsinghbabbar@gmail.com>
2025-03-07 21:39:32 +0530

    Created Shape class

---
+++ Shape.java
@@ -0,0 +1,33 @@
+// Shape.java
+
+abstract class Shape
+{
+	protected double dim_one;
+	protected double dim_two;
+	protected double dim_three;
+	protected int num_sides;
+	
+	public Shape()
+	{
+		dim_one = dim_two = dim_three = 0;
+		num_sides = 0;
+	}
+	// to prevent exception
+	
+	public Shape(double dim_one, double dim_two, double dim_three, int num_sides)
+	{
+		this.dim_one = dim_one;
+		this.dim_two = dim_two;
+		this.dim_three = dim_three;
+		this.num_sides = num_sides;
+	}
+	
+	abstract public double calculateArea();
+	abstract public double calculatePerimeter();
+	//abstract methods
+	
+	public int getNumSides()
+	{
+		return num_sides;
+	}
+}
