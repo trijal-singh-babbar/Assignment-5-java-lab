1cccc409daa5e01fd3c4be35bd0c6710d5af82ac
trijal-singh-babbar <trijalsinghbabbar@gmail.com>
2025-03-07 21:40:53 +0530

    Created Triangle Class

---
+++ Triangle.java
@@ -0,0 +1,19 @@
+// Triangle.java
+
+class Triangle extends Shape
+{
+	public Triangle(double base, double height)
+	{
+		super(base,height,0,3);
+	}
+	
+	public double calculateArea()
+	{
+		return 0.5 * dim_one * dim_two;
+	}
+	
+	public double calculatePerimeter()
+	{
+		return (dim_one + dim_two) + Math.sqrt((dim_one * dim_one) + (dim_two * dim_two));
+	}
+}
